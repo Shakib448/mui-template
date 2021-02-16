946c0e990957bfb53417e5157cb3a5bffded1a2c
Shakib448 <shakiba448@gmail.com>
2021-02-16 16:07:41 +0600

    WIP on THEME

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-function App() {
+import React, { useState } from "react";
+
+import { Button, ButtonGroup, Checkbox } from "@material-ui/core";
+
+const App = () => {
+  const [checked, setChecked] = useState(true);
+  console.log(checked);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "lightgray",
+      }}
+    >
+      <Checkbox
+        checked={checked}
+        onChange={(e) => setChecked(e.target.checked)}
+      />
+      <ButtonGroup variant="contained" color="primary">
+        <Button>Hello World</Button>
+        <Button>Hello World</Button>
+      </ButtonGroup>
     </div>
   );
-}
+};
 
 export default App;

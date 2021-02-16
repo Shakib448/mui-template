12127de5b6ced4babe4419716092f34cd2f04f94
Shakib448 <shakiba448@gmail.com>
2021-02-16 17:17:10 +0600

    WIP on THEME

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import {
   Checkbox,
   FormControlLabel,
   makeStyles,
+  ThemeProvider,
+  createMuiTheme,
   TextField,
 } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import SaveIcon from "@material-ui/icons/Save";
+import { green } from "@material-ui/core/colors";
 
 const useStyle = makeStyles({
   root: {
@@ -20,47 +23,57 @@ const useStyle = makeStyles({
   },
 });
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: green[500],
+    },
+  },
+});
+
 const App = () => {
   const [checked, setChecked] = useState(true);
   const classes = useStyle();
 
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        background: "lightgray",
-        flexDirection: "column",
-      }}
-    >
-      <TextField
-        variant="filled"
-        color="secondary"
-        type="email"
-        label="Email"
-        placeholder="test@gmail.com"
-      />
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={checked}
-            color="primary"
-            onChange={(e) => setChecked(e.target.checked)}
-            inputProps={{ "aria-label": "secondary checkbox" }}
-            icon={<DeleteIcon />}
-            checkedIcon={<SaveIcon />}
-          />
-        }
-        label="Testing Checkbox"
-      />
-      <ButtonGroup variant="contained" color="primary">
-        <Button>Hello World</Button>
-        <Button>Hello World</Button>
-      </ButtonGroup>
-      <Button className={classes.root}>Test style button</Button>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "lightgray",
+          flexDirection: "column",
+        }}
+      >
+        <TextField
+          variant="filled"
+          color="secondary"
+          type="email"
+          label="Email"
+          placeholder="test@gmail.com"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={checked}
+              color="primary"
+              onChange={(e) => setChecked(e.target.checked)}
+              inputProps={{ "aria-label": "secondary checkbox" }}
+              icon={<DeleteIcon />}
+              checkedIcon={<SaveIcon />}
+            />
+          }
+          label="Testing Checkbox"
+        />
+        <ButtonGroup variant="contained" color="primary">
+          <Button>Hello World</Button>
+          <Button>Hello World</Button>
+        </ButtonGroup>
+        <Button className={classes.root}>Test style button</Button>
+      </div>
+    </ThemeProvider>
   );
 };
 

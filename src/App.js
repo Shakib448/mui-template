353055111156f24bdcce3b41d04595dd990d71fa
Shakib448 <shakiba448@gmail.com>
2021-02-16 16:21:03 +0600

    WIP on THEME

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 
-import { Button, ButtonGroup, Checkbox } from "@material-ui/core";
+import {
+  Button,
+  ButtonGroup,
+  Checkbox,
+  FormControlLabel,
+} from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import SaveIcon from "@material-ui/icons/Save";
 
 const App = () => {
   const [checked, setChecked] = useState(true);
@@ -13,11 +20,21 @@ const App = () => {
         alignItems: "center",
         justifyContent: "center",
         background: "lightgray",
+        flexDirection: "column",
       }}
     >
-      <Checkbox
-        checked={checked}
-        onChange={(e) => setChecked(e.target.checked)}
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={checked}
+            color="primary"
+            onChange={(e) => setChecked(e.target.checked)}
+            inputProps={{ "aria-label": "secondary checkbox" }}
+            icon={<DeleteIcon />}
+            checkedIcon={<SaveIcon />}
+          />
+        }
+        label="Testing Checkbox"
       />
       <ButtonGroup variant="contained" color="primary">
         <Button>Hello World</Button>
